e06830ec640d733fc7435bec8c558c1245309a19
Luke Chadwick <me@vertis.io>
2016-11-26 23:15:53 +1100

    Further touch handling

--- Assets/FieldContextHandler.cs
+++ Assets/FieldContextHandler.cs
@@ -1,21 +1,51 @@
 ﻿using UnityEngine;
 using System.Collections;
 using TouchScript;
+using TouchScript.Gestures;
+using System;
 
 public class FieldContextHandler : MonoBehaviour {
 //	public float speed = 0.1F;
 //	// Update is called once per frame
 	public GameObject cornPrefab;
 
-	void Update () {
-		if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began) {
-			// Get movement of the finger since last frame
-			//Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
-			// Move object across XY plane
-			//transform.Translate(-touchDeltaPosition.x * speed, -touchDeltaPosition.y * speed, 0);
-			transform.Find("Canvas").gameObject.SetActive(true);
-		}
+//	void Update () {
+//		if(Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began) {
+//			// Get movement of the finger since last frame
+//			//Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
+//			// Move object across XY plane
+//			//transform.Translate(-touchDeltaPosition.x * speed, -touchDeltaPosition.y * speed, 0);
+//			transform.Find("Canvas").gameObject.SetActive(true);
+//		}
+//
+//		if(Input.GetMouseButtonDown(0))
+//		{
+////			Debug.Log("Pressed left click.");
+////			transform.Find("Canvas").gameObject.SetActive(true);
+//			RaycastHit hit; 
+//			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); 
+//			if ( Physics.Raycast(ray,out hit,100.0f)) {
+//				Debug.Log("You selected the " + hit.transform.name); // ensure you picked right object
+//				Debug.DrawRay(hit.point, hit.normal, Color.green, 2, false);
+//			}
+//		}
+//	}
+
+	private void OnEnable()
+	{
+		GetComponent<TapGesture>().Tapped += tapHandler;
 	}
+
+	private void OnDisable()
+	{
+		GetComponent<TapGesture>().Tapped -= tapHandler;
+	}
+
+	private void tapHandler(object sender, EventArgs e)
+	{
+		transform.Find("Canvas").gameObject.SetActive(true);
+	}
+
 
 	public void OnClickCorn() {
 		transform.Find("Canvas").gameObject.SetActive(false);
---
+++ Assets/FieldsHandler.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FieldsHandler : MonoBehaviour {
+
+	public GameObject fieldPrefab;
+	public int rows;
+	public int columns;
+	private int size = 4;
+	// Use this for initialization
+	void Start () {
+		for(var z = 0; z < rows; z++) {
+			for(var x = 0; x < rows; x++) {
+				GameObject cornObject = Instantiate(fieldPrefab, new Vector3(transform.position.x,transform.position.y, transform.position.z), Quaternion.identity) as GameObject;
+				cornObject.transform.parent = gameObject.transform;
+				cornObject.transform.localPosition = new Vector3(size * x, 0, size * z);
+			}
+		}
+	}
+}
