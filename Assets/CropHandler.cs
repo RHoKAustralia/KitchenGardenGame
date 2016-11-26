365c07a8d5faa49d8a3deb23746304233d032330
Luke Chadwick <me@vertis.io>
2016-11-26 23:47:18 +1100

    Progress over time

---
+++ Assets/CropHandler.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CropHandler : MonoBehaviour {
+
+	float startTime;
+	private float growthTime = 10;
+	private Slider growthSlider;
+	// Use this for initialization
+	void OnEnable () {
+		startTime = Time.time;
+		growthSlider = transform.Find("Canvas/GrowthSlider").GetComponent<Slider>();
+		growthSlider.value = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log("Time: " + growthTime + " " + startTime + " " + Time.time);
+		float completionPercent = ((Time.time - startTime) / growthTime) * 100f;
+		if(growthSlider && completionPercent < 100) {
+			growthSlider.value = completionPercent;
+		}
+		if(completionPercent >= 100) {
+			gameObject.SetActive(false);
+		}
+	}
+}
