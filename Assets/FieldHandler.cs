8cec197077df8e35a26778e18549df3495f1ae40
Luke Chadwick <me@vertis.io>
2016-11-26 17:13:40 +1100

    Handle clicking on one of the fields, take 2

---
+++ Assets/FieldHandler.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FieldHandler : MonoBehaviour {
+
+	GameObject cornField;
+	// Use this for initialization
+	void Start () {
+		cornField = gameObject.transform.Find("Corn Field").gameObject;
+		cornField.SetActive(false);
+	}
+
+	void OnMouseDown() {
+		if(cornField.activeInHierarchy) {
+			cornField.SetActive(false);
+		} else {
+			cornField.SetActive(true);
+		}
+	}
+}
