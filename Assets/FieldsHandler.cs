abb81eda808c6cc2c936fe375295a9a73a8a7d84
Luke Chadwick <me@vertis.io>
2016-11-27 11:46:00 +1100

    Bug on column generation

--- Assets/FieldsHandler.cs
+++ Assets/FieldsHandler.cs
@@ -10,7 +10,7 @@ public class FieldsHandler : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		for(var z = 0; z < rows; z++) {
-			for(var x = 0; x < rows; x++) {
+			for(var x = 0; x < columns; x++) {
 				GameObject cornObject = Instantiate(fieldPrefab, new Vector3(transform.position.x,transform.position.y, transform.position.z), Quaternion.identity) as GameObject;
 				cornObject.transform.parent = gameObject.transform;
 				cornObject.transform.localPosition = new Vector3(size * x, 0, size * z);
