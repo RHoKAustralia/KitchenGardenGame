9e0fd97ee61a0c080084b24bb349890acf6fd5e7
Luke Chadwick <me@vertis.io>
2017-01-06 18:27:50 -0800

    Final assets

--- Assets/CropHandler.cs
+++ Assets/CropHandler.cs
@@ -5,18 +5,21 @@ using UnityEngine.UI;
 public class CropHandler : MonoBehaviour {
 
 	float startTime;
-	private float growthTime = 10;
+	public float growthTime = 10;
 	private Slider growthSlider;
+	private Slider healthSlider;
 	// Use this for initialization
 	void OnEnable () {
 		startTime = Time.time;
 		growthSlider = transform.Find("Canvas/GrowthSlider").GetComponent<Slider>();
 		growthSlider.value = 0;
+		healthSlider = transform.Find("Canvas/HealthSlider").GetComponent<Slider>();
+		healthSlider.value = 100;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Debug.Log("Time: " + growthTime + " " + startTime + " " + Time.time);
+		//Debug.Log("Time: " + growthTime + " " + startTime + " " + Time.time);
 		float completionPercent = ((Time.time - startTime) / growthTime) * 100f;
 		if(growthSlider && completionPercent < 100) {
 			growthSlider.value = completionPercent;
@@ -24,5 +27,16 @@ public class CropHandler : MonoBehaviour {
 		if(completionPercent >= 100) {
 			gameObject.SetActive(false);
 		}
+		if(healthSlider.value <= 0) {
+			Destroy(gameObject);
+		}
+	}
+		
+	void OnTriggerStay(Collider col) {
+		Debug.Log("Collision");
+		healthSlider.value = healthSlider.value - 1;
+//		foreach(ContactPoint contact in collision.contacts) {
+//			Debug.DrawRay(contact.point, contact.normal, Color.white);
+//		}
 	}
 }
--- Assets/FieldHandler.cs
+++ Assets/FieldHandler.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class FieldHandler : MonoBehaviour {
 
 	GameObject cornField;
+
 	// Use this for initialization
 	void Start () {
 		cornField = gameObject.transform.Find("Corn Field").gameObject;
